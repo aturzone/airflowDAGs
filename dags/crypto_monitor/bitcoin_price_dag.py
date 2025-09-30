630ba5bfc98b29b83974053d51d122e13c61cd35
aturzone <aturzone@gmail.com>
2025-09-30 18:21:06 +0330

    python

--- dags/crypto_monitor/bitcoin_price_dag.py
+++ dags/crypto_monitor/bitcoin_price_dag.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator # type: ignore
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import Variable
 import requests
